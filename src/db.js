84f4b440d7daa977f5e2570aee2d20e43045142d
sntseason-lgtm <sntseason@gmail.com>
2026-01-29 23:24:55 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
+    console.error("❌ MongoDB error:", error.message);
     process.exit(1);
   }
 };
 
-export default connect
+export default connectDB;
