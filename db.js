c9fd151d4473586611e2354041c3538b2dd7d866
sntseason-lgtm <sntseason@gmail.com>
2026-02-11 07:35:16 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    console.log("MongoDB connected ✅");
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.log("MongoDB connection error:", err.message);
   }
 };
 
