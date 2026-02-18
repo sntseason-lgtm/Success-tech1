f11af5ed0216d538e9bd81803583a46e7481342c
sntseason-lgtm <sntseason@gmail.com>
2026-02-19 00:19:53 +0100

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export default function auth(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: "No token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.admin = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+                               }
